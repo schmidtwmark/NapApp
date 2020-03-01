7427dfbf380c7a1c182a9baff509d566603eb286
Mark Schmidt <schmidtwmark@gmail.com>
2020-02-29 21:19:24 -0600

    Initial Commit

---
+++ NapApp WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  NapApp WatchKit Extension
+//
+//  Created by Mark Schmidt on 2/29/20.
+//  Copyright © 2020 Mark Schmidt. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
