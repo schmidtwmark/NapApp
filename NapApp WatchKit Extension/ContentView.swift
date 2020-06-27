50ba9442c2d04773bd878cb5cc91137aeaa8dae2
Mark Schmidt <schmidtwmark@gmail.com>
2020-06-27 07:13:25 -0500

    Finish first view

--- NapApp WatchKit Extension/ContentView.swift
+++ NapApp WatchKit Extension/ContentView.swift
@@ -9,29 +9,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var times = [20, 30, 40, 50, 60, 75, 90, 120]
+    @State private var selectedTimeIndex = 0;
+    
     var body: some View {
-        Group {
-            Text("Hello Jam")
+        
+        
+        GeometryReader() {
+            geometry in self.TimerView(geometry: geometry)
+
+
         }
-        .contextMenu(menuItems: getMenuItems())
+        .edgesIgnoringSafeArea([.bottom])
     }
     
-}
-
-func getMenuItems() -> View {
-    return {
-        Button(action: {
-            print("Refresh")
-        }, label: {
-            VStack{
-                Image(systemName: "arrow.clockwise")
-                    .font(.title)
-                Text("Refresh view")
+    private func TimerView(geometry: GeometryProxy) -> some View {
+        return
+            VStack {
+                Text("Set Nap Length").frame(maxWidth: .infinity, alignment: .leading)
+    
+                Picker(selection: self.$selectedTimeIndex, label: Text("")) {
+                    ForEach(0 ..< self.times.count) {
+                        Text(String(self.times[$0]) + " minutes")
+                    }
+                }.padding(EdgeInsets.init(top: 0, leading: 0, bottom: 0, trailing: 0))
+    
+                Button("Start", action: {
+                    let time = self.times[self.selectedTimeIndex]
+                    print("Start Pressed, selected time " + String(time))
+                })
+                .accentColor(Color.green)
+                    // Frame uses bottom safe area because side safe area is only 0.5 for some unknown reason on series 4/5
+                .frame(width: geometry.frame(in: .global).width - geometry.safeAreaInsets.bottom, height: 44, alignment: Alignment(horizontal: .center, vertical: .center))
+//
             }
-        })
     }
-        
-        
+    
 }
 
 
